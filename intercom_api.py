ace2287936ec539d1f25e2c29e559173ce7335de
Philip <philipthomas7394@gmail.com>
2018-01-29 18:33:38 -0600

    added comments

--- intercom_api.py
+++ intercom_api.py
@@ -1,24 +1,25 @@
 import requests
 import MySQLdb
-import json
 
+# db connection details
 hostname = 'localhost'
 username = 'root'
 password = 'admin123'
 database = 'intercomDB'
 
+# executes db query
 def doQuery( conn ) :
-    cur = conn.cursor()
+    cur = conn.cursor() # get cursor
     cur.execute( "SELECT id, name, email FROM user" )
     
-    return cur.fetchall()
+    return cur.fetchall() # return the result set
         
-
+# main
 if __name__ == '__main__':
 
-	myConnection = MySQLdb.connect(host=hostname, user=username, passwd=password, db=database)
+	myConnection = MySQLdb.connect(host=hostname, user=username, passwd=password, db=database) # open connection
 	result_set = doQuery(myConnection)
-	myConnection.close()
-	headers = {'Authorization': 'Bearer <Your access token>', 'Accept': 'application/json', 'Content-type': 'application/json'}
+	myConnection.close() # close connection
+	headers = {'Authorization': 'Bearer <Your access token>', 'Accept': 'application/json', 'Content-type': 'application/json'} # request header
 	for id, email, name in result_set:
-		requests.post('https://api.intercom.io/users', headers=headers, json={"user_id": id, "email": email, "name": name})
+		requests.post('https://api.intercom.io/users', headers=headers, json={"user_id": id, "email": email, "name": name})# send post request with json data
